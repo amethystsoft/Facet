3aef7517896e076c530d51ebe585eeaa39ad1a5c
Mia Koring <koring.mia@gmail.com>
2026-06-28 23:31:23 +0200

    Initial Commit

---
+++ Facet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Facet
+//
+//  Created by Mia Koring on 28.06.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Facet/FacetApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacetApp.swift
+//  Facet
+//
+//  Created by Mia Koring on 28.06.26.
+//
+
+import SwiftUI
+
+@main
+struct FacetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
